36a3b74ce58273650c64d2830514fb20c85bc10c
unknown <josharms00@gmail.com>
2020-11-07 21:27:24 -0500

    Added nav mesh for computer player to chase and run away from player 1.

---
+++ Assets/Scripts/CPU/ChasePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+
+    Transform player;
+    NavMeshAgent nav;
+    float distanceAway;
+
+    Tagged tagged;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+        nav = GetComponent<NavMeshAgent>();
+
+        tagged = GetComponent<Tagged> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if(tagged.It)
+        {
+            nav.SetDestination(player.position);
+        }
+        else
+        {
+            nav.SetDestination(10*player.forward);
+        }
+        
+        
+    }
+}
